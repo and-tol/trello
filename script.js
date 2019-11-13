a2fcc4d4c9c6bbcd4ca9748e69fdf742b8f16fad
andreitol <tolstoi.a@gmail.com>
2019-11-13 22:06:53 +0200

    fix bag in note

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-let columnIdCounter = 4;
-
 // create new empty note
 document
   .querySelectorAll(".column")
   // 'button' for create new note is in each column
-  .forEach(columnProcess);
+  .forEach(Column.process);
 
 // create new empty column
 document
@@ -15,10 +13,10 @@ document
     const columnElement = document.createElement("div");
     columnElement.classList.add("column");
     columnElement.setAttribute("draggable", "true");
-    columnElement.setAttribute("data-column-id", `${columnIdCounter}`);
+    columnElement.setAttribute("data-column-id", `${Column.idCounter}`);
 
     columnElement.innerHTML = `
-    <p class="column-header">
+      <p class="column-header">
         В плане
       </p>
       <div data-notes></div>
@@ -29,59 +27,12 @@ document
       </p>
     `;
     // count id for the new column
-    columnIdCounter++;
+    Column.idCounter++;
 
     // add new column
     document.querySelector(".columns").append(columnElement);
-    columnProcess(columnElement);
+    Column.process(columnElement);
   });
 
 // editable notes
 document.querySelectorAll(".note").forEach(Note.process);
-
-// function for processing of column
-function columnProcess(columnElement) {
-  // this is 'button' for create new note
-  const spanAction_addNote = columnElement.querySelector(
-    "[data-action-addNote]"
-  );
-
-  // create new note for column with click
-  spanAction_addNote.addEventListener("click", function(event) {
-    const noteElement = document.createElement("div");
-    noteElement.classList.add("note");
-    noteElement.setAttribute("draggable", "true");
-    noteElement.setAttribute("data-note-id", `${Note.idCounter}`);
-
-    // count id for the new note
-    Note.idCounter++;
-
-    // add new note to column
-    columnElement.querySelector("[data-notes]").append(noteElement);
-    Note.process(noteElement);
-
-    noteElement.setAttribute("contenteditable", "true");
-    noteElement.focus();
-  });
-
-  const headerElement = columnElement.querySelector(".column-header");
-  headerElement.addEventListener("dblclick", function(event) {
-    headerElement.setAttribute("contenteditable", "true");
-    headerElement.focus();
-  });
-
-  headerElement.addEventListener("blur", function(event) {
-    headerElement.removeAttribute("contenteditable");
-  });
-
-  // drop note on column
-  columnElement.addEventListener("dragover", function(event) {
-    event.preventDefault();
-  });
-
-  columnElement.addEventListener("drop", function(event) {
-    if (Note.dragged) {
-      return columnElement.querySelector("[data-notes]").append(Note.dragged);
-    }
-  });
-}
